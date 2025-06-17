89a215168fc0df4757c55827cefeff4a79027d3d
SinghalRish <singhalrishabh18@gmail.com>
2025-06-17 16:47:39 +0530

    Add dynamic name in login-header

--- src/app/shared/loginheader/loginheader.component.ts
+++ src/app/shared/loginheader/loginheader.component.ts
@@ -9,7 +9,25 @@ import { Route, Router } from '@angular/router';
 })
 export class LoginheaderComponent {
   constructor(private router: Router) {}
-   navigate(){
-    this.router.navigate(['/home'])
-   }
+  fullName: string = 'Kenny Sebastian'; // can be dynamic
+  initials: string = '';
+
+  ngOnInit() {
+    this.initials = this.getInitials(this.fullName);
+  }
+
+  getInitials(name: string): string {
+    const nameParts = name.trim().split(' ').filter(Boolean);
+
+    if (nameParts.length === 1) {
+      return nameParts[0][0].toUpperCase();
+    }
+
+    const firstInitial = nameParts[0][0].toUpperCase();
+    const lastInitial = nameParts[nameParts.length - 1][0].toUpperCase();
+    return firstInitial + lastInitial;
+  }
+  navigate() {
+    this.router.navigate(['/home']);
+  }
 }
